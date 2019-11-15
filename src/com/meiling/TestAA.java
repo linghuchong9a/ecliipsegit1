bde1e0e41af6aa6a18342d47c3e67cfe8481c5d3
linghuzhonga9 <1158089754@qq.com>
2019-11-15 16:26:37 +0800

    git项目 内容首次添加

--- src/com/meiling/TestAA.java
+++ src/com/meiling/TestAA.java
@@ -4,6 +4,7 @@ public class TestAA {
 
 	public static void main(String[] args) {
 	System.out.println("----------Git项目完成------------");
+	System.out.println("111111111111Git项目完成------------");
 
 	}
 

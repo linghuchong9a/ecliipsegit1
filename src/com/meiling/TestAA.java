05df393d9d9abf19fabee726f6655bc4589a2fab
meiling211 <2876277418@qq.com>
2019-11-15 19:37:49 +0800

    222修改

--- src/com/meiling/TestAA.java
+++ src/com/meiling/TestAA.java
@@ -4,7 +4,7 @@ public class TestAA {
 
 	public static void main(String[] args) {
 	System.out.println("----------Git项目完成------------");
-	System.out.println("111111111111Git项目完成------------");
+	System.out.println("11111121111Git项目完成------------");
 	System.out.println("222456Git项目完成------修改后------");
 
 	}
